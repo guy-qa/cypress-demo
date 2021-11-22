fff7bf107e63f77d97c4a997b33334bf2312a542
Umair <umair11cp06@gmail.com>
2021-11-22 17:20:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# cypress_basic_demo
+
+commands to run the tests via Script
+
+1) npm install
+2) npm run start
+3) npm run test
+
+commands to run the tests directly via cypress
+
+1) npx cypress run 
+
+http://localhost:3000/ to see the UI without test.
---
+++ testApp.js
@@ -0,0 +1,20 @@
+const cypress = require('cypress')
+
+cypress.run({
+  spec: './cypress/integration/searchPerson.spec.js',
+  reporter: 'junit',
+  browser: 'chrome',
+  config: {
+    baseUrl: 'http://localhost:3000'
+  },
+  env: {
+    foo: 'bar',
+    baz: 'quux',
+  }
+})
+  .then((results) => {
+    console.log("fim")
+  })
+  .catch((err) => {
+    console.error(err)
+  })
